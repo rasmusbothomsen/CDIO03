799bd1da401942b8e8224c4e5a742f95f379b6b3
Fink <christo.fink.1@gmail.com>
2021-11-08 23:07:52 +0100

    kommentar

--- Matador/main/java/run.java
+++ Matador/main/java/run.java
@@ -1,4 +1,4 @@
-//kun til at køre applikationen må IKKE indholde metoder!!! - for mindre coupling og højere sammenhørlighed ved packages//
+//kun til at køre applikationen må IKKE indholde metoder!!! - for mindre kopling/interferens og højere sammenhørlighed ved packages//
 
 public class run {
 }
