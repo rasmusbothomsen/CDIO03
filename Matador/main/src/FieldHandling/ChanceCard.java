985221522675c8ecc9a746fc5b2661deada2933f
Rasmus Thomsen <rasmus0408@gmail.com>
2021-11-09 11:17:08 +0100

    Oprettet getter og setter for ChanceCard

--- Matador/main/src/FieldHandling/ChanceCard.java
+++ Matador/main/src/FieldHandling/ChanceCard.java
@@ -11,4 +11,27 @@ public class ChanceCard {
     protected String cardDescription;
     protected int effectOnPlayer;
 
+    public String getNameOfCard() {
+        return nameOfCard;
+    }
+
+    public void setNameOfCard(String nameOfCard) {
+        this.nameOfCard = nameOfCard;
+    }
+
+    public String getCardDescription() {
+        return cardDescription;
+    }
+
+    public void setCardDescription(String cardDescription) {
+        this.cardDescription = cardDescription;
+    }
+
+    public int getEffectOnPlayer() {
+        return effectOnPlayer;
+    }
+
+    public void setEffectOnPlayer(int effectOnPlayer) {
+        this.effectOnPlayer = effectOnPlayer;
+    }
 }
