e235b0c28909a131c03101721778f29c2c2628fa
Rasmus Thomsen <rasmus0408@gmail.com>
2021-11-09 13:28:10 +0100

    CardDeck læser nu fra tekstFilen, og opretter kort tilsvarende

--- Matador/main/src/FieldHandling/CardDeck.java
+++ Matador/main/src/FieldHandling/CardDeck.java
@@ -5,17 +5,32 @@ import TextReader.TextFileReader;
 
 public class CardDeck {
 
-    protected ChanceCard[] cardDeck;
+    private ChanceCard[] cardDeck;
 
-
+    public CardDeck() {
+        this.cardDeck = cardDeckFiller();
+    }
 
     protected ChanceCard[] cardDeckFiller() {
         TextFileReader reader = new TextFileReader("ChanceCardText.txt",25);
-        String[] cards = reader.fileReader();
-        System.out.println(cards[3]);
+        String[] cardsText = reader.fileReader();
+        int amoutOfUnuseableText=0;
+        for (String s : cardsText) {
+            if (s.startsWith("Card")) {
+                amoutOfUnuseableText++;
+            }
+        }
+        ChanceCard[] cards = new ChanceCard[cardsText.length-amoutOfUnuseableText];
+        for(int i =0, a=0; i<cards.length;i++,a++){
+            if(cardsText[i].startsWith("Card")) {
+                a++;
+            }
+            cards[i]= new ChanceCard(cardsText[a],cardsText[a+1],i);
+
+        }
 
 
 
-    return null;
+    return cards;
     }
 }
