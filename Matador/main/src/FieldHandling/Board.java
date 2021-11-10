ce082921c30c2c41eaf89023eb0fadb8ab43055c
Rasmus Thomsen <rasmus0408@gmail.com>
2021-11-10 13:12:12 +0100

    Board klassen bliver lavet, har ændret Amusement og slået den sammen med Fields super klassen

--- Matador/main/src/FieldHandling/Board.java
+++ Matador/main/src/FieldHandling/Board.java
@@ -1,8 +1,29 @@
 package FieldHandling;
+import TextReader.TextFileReader;
 
 public class Board {
     private Amusement[] amusements;
-    private String[][] fieldDescription;
+    private Field[] fields;
     private CardDeck chanceDeck;
 
+    public Board() {
+        fillArrays();
+        this.fields=fields;
+        this.amusements= amusements;
+        this.chanceDeck = new CardDeck();
+    }
+    private void fillArrays(){
+        TextFileReader reader = new TextFileReader("FieldsText.txt",25);
+        fields=new Field[24];
+        amusements=new Amusement[16];
+
+
+
+    }
+    private Field[] fieldsArrayFiller(int i) {
+        return null;
+    }
+    private Amusement[] amusementsArrayFiller(int i){
+        return null;
+    }
 }
---
+++ Matador/main/src/FieldHandling/Field.java
@@ -0,0 +1,100 @@
+package FieldHandling;
+
+import TurnHandling.Player;
+
+public class Field {
+    private String fieldName;
+    private String fieldDiscription;
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public String getFieldDiscription() {
+        return fieldDiscription;
+    }
+
+    public int getPlacementOnBoard() {
+        return placementOnBoard;
+    }
+
+    private int placementOnBoard;
+
+    public Field(String fieldName, String fieldDiscription, int placementOnBoard) {
+        this.fieldName = fieldName;
+        this.fieldDiscription = fieldDiscription;
+        this.placementOnBoard = placementOnBoard;
+    }
+}
+
+
+class Chance extends Field{
+
+
+    public Chance(String fieldName, String fieldDiscription, int placementOnBoard) {
+        super(fieldName, fieldDiscription, placementOnBoard);
+    }
+}
+
+class Amusement extends Field{
+    private int cost;
+    private Player playerwhoOwnsIt;
+    private Amusement sameType;
+    private boolean isOwned;
+    private boolean allIsOwned;
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setPlayerwhoOwnsIt(Player playerwhoOwnsIt) {
+        this.playerwhoOwnsIt = playerwhoOwnsIt;
+    }
+
+    public Player getPlayerwhoOwnsIt() {
+        return playerwhoOwnsIt;
+    }
+
+    public Amusement getSameType() {
+        return sameType;
+    }
+
+    public boolean isOwned() {
+        return isOwned;
+    }
+
+    public boolean isAllIsOwned() {
+        return allIsOwned;
+    }
+
+    public Amusement(String fieldName, String fieldDiscription, int placementOnBoard,
+                     int cost, Amusement sameType, boolean isOwned, boolean allIsOwned ) {
+        super(fieldName, fieldDiscription, placementOnBoard);
+        this.cost = cost;
+        this.sameType = sameType;
+        this.isOwned = isOwned;
+        this.allIsOwned = allIsOwned;
+    }
+
+
+    class RestRoom extends Field{
+
+        public RestRoom(String fieldName, String fieldDiscription, int placementOnBoard) {
+            super(fieldName, fieldDiscription, placementOnBoard);
+        }
+    }
+
+    class GoToRestRoom extends Field{
+
+        public GoToRestRoom(String fieldName, String fieldDiscription, int placementOnBoard) {
+            super(fieldName, fieldDiscription, placementOnBoard);
+        }
+    }
+
+    class Start extends Field{
+
+        public Start(String fieldName, String fieldDiscription, int placementOnBoard) {
+            super(fieldName, fieldDiscription, placementOnBoard);
+        }
+    }
+}
