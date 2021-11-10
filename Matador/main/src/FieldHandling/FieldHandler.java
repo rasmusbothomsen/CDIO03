da710ef55fa02facf46a6b2c31ce6c29c96cca1e
Rasmusbothomsen <rasmus0408@gmai.com>
2021-11-10 21:44:47 +0100

    Opretter fieldHandler

--- Matador/main/src/FieldHandling/FieldHandler.java
+++ Matador/main/src/FieldHandling/FieldHandler.java
@@ -1,7 +1,55 @@
 package FieldHandling;
 
+import TurnHandling.Player;
+
 public class FieldHandler {
     private CardDeck cardDeck;
     private Board board;
-    private Amusement amusement;
+
+    public FieldHandler() {
+        this.cardDeck = new CardDeck();
+        this.board = new Board();
+    }
+    public void initiateField(Player player){
+        Object[] fields = board.getFields();
+        if(fields[player.getPlacementONBoard()].getClass().equals(Amusement.class)){
+            isAmusement(player,fields);
+        }
+        if(fields[player.getPlacementONBoard()].getClass().equals(Chance.class)){
+            isChance(player,fields);
+        }
+        if(fields[player.getPlacementONBoard()].getClass().equals(RestRoom.class)){
+            isRestroom(player,fields);
+        }
+        if(fields[player.getPlacementONBoard()].getClass().equals(Field.class)){
+            isField(player,fields);
+        }
+
+
+
+
+    }
+    private void isAmusement(Player player, Object[] fields){
+       Amusement amusement= ((Amusement) fields[player.getPlacementONBoard()]);
+       if(amusement.isOwned()){
+           if(amusement.isAllIsOwned()){
+               player.addMoney(amusement.getCost()*-2);
+           }else{
+               player.addMoney(amusement.getCost()*-1);
+           }
+       }else{
+           player.addMoney(amusement.getCost()*-1);
+           amusement.setPlayerwhoOwnsIt(player);
+       }
+    }
+    private void isChance(Player player, Object[] fields){
+
+    }
+    private void isRestroom(Player player, Object[] fields){
+
+    }
+    private void isField(Player player,Object[] fields){
+
+    }
+
 }
--- Matador/test/TestSrc/FieldHandling/BoardTest.java
+++ Matador/test/TestSrc/FieldHandling/BoardTest.java
@@ -10,9 +10,16 @@ class BoardTest {
         Object[] fieldsTest = board.getFields();
         System.out.println(board);
         System.out.println(board.toClasses());
+        System.out.println();
     }
-@Test
-    void testAbstract(){
+    @Test
+    void testClassEquals(){
+        Board board = new Board();
+        Object[] fieldsTest = board.getFields();
+        System.out.println(fieldsTest[3].getClass().equals(Chance.class));
+        System.out.println(fieldsTest[2].getClass().equals(Amusement.class));
+        System.out.println(fieldsTest[3].getClass().equals(Chance.class));
+
 
 
     }
