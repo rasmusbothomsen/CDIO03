3877313bc50e3be0531b6a63778a6c609105dadf
Rasmus Thomsen <rasmus0408@gmail.com>
2021-11-16 13:01:06 +0100

    Oprettet midlertidigt, gui, mangler stadig farver og tekst på den

--- src/main/java/Controllers/GUIcreator.java
+++ src/main/java/Controllers/GUIcreator.java
@@ -1,40 +1,46 @@
 package Controllers;
 import FieldHandling.*;
-import gui_fields.GUI_Field;
-import gui_fields.GUI_Street;
+import gui_fields.*;
 import gui_main.GUI;
 
 import java.awt.*;
 
 public class GUIcreator {
     public static void main(String[] args) {
-        GUI gui = new GUI();
-
+        GUI gui = new GUI(guiCreator());
     }
 
-    public static GUI guiCreator() {
+    public static GUI_Field[] guiCreator() {
         Board board = new Board();
         Object[] fields = board.getFields();
         GUI_Field[] guiFields = fieldsCreator(fields);
-return null;
+        return fieldsCreator(fields);
+
     }
     private static GUI_Field[] fieldsCreator(Object[] fieldFromBoard){
         GUI_Field[] guiFields = new GUI_Field[24];
         for (int i =0; i<fieldFromBoard.length;i++){
                 if(fieldFromBoard[i].getClass().equals(Amusement.class)){
-                    guiFields[i] = new GUI_Street(((Amusement)fieldFromBoard[i]).getFieldName(),((Amusement) fieldFromBoard[i]).getFieldDiscription(),"heh", Integer.toString(((Amusement) fieldFromBoard[i]).getCost()), Color.GRAY,Color.PINK );
+                    guiFields[i] = new GUI_Street(((Amusement)fieldFromBoard[i]).getFieldName(),Integer.toString(((Amusement) fieldFromBoard[i]).getCost())+"$",
+                            ((Amusement)fieldFromBoard[i]).getFieldDiscription(), Integer.toString(((Amusement) fieldFromBoard[i]).getCost()), Color.GRAY,Color.PINK );
                 }
             if(fieldFromBoard[i].getClass().equals(Chance.class)){
+                guiFields[i]= new GUI_Chance("?",((Chance)fieldFromBoard[i]).getFieldName(),"Chance"
+                        ,Color.WHITE,Color.BLACK);
 
             }
             if(fieldFromBoard[i].getClass().equals(RestRoom.class)){
+                if(((RestRoom)fieldFromBoard[i]).isGoTo()){
+                    guiFields[i]=new GUI_Jail(null,((Field) fieldFromBoard[i]).getFieldName(),((Field) fieldFromBoard[i]).getFieldDiscription(),"Go to",Color.GRAY,Color.BLUE);
+                }else{
+                    guiFields[i]=new GUI_Jail(null,((Field) fieldFromBoard[i]).getFieldName(),((Field) fieldFromBoard[i]).getFieldDiscription(),"Visit",Color.GRAY,Color.BLUE);
+                }
 
             }
             if(fieldFromBoard[i].getClass().equals(Field.class)){
-
+                guiFields[i]=new GUI_Street();
             }
         }
-
-return null;
+return guiFields;
     }
 }
